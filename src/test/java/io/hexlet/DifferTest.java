fcb8e63c7f7925786ee7ebd814ee7c4d1496f05f
Maksim Litvinov <maksim.litvinov@hexlet.io>
2022-06-06 17:35:20 +0300

    add tests

---
+++ src/test/java/io/hexlet/DifferTest.java
@@ -0,0 +1,41 @@
+package io.hexlet;
+
+import hexlet.code.Differ;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DifferTest {
+
+    private static String expectedStylish;
+    private static String basePath = "src/test/resources/fixtures/";
+
+    @BeforeAll
+    static void beforeAll() throws Exception {
+        expectedStylish = Files.readString(getFixturePath("stylish"));
+    }
+
+    @Test
+    void testJsonStylish() throws Exception {
+        var actual = Differ.generate(
+                basePath + "file1.json",
+                basePath + "file2.json",
+                "stylish"
+        );
+        assertThat(actual).isEqualTo(expectedStylish);
+
+
+
+    }
+
+    private static Path getFixturePath(String fileName) {
+        return Paths.get(basePath + fileName).toAbsolutePath().normalize();
+    }
+
+
+}
